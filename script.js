b3da811da700bdb731bc40f786f70d8f3c03a44e
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-31 14:10:55 -0300

    Ajustando responsividade, removendo animação padrão de clique

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const buttonMenu = document.querySelector('#btn-mobile');
 
-buttonMenu.addEventListener('click', () => {
+const toggleMenu = (event) => {
+  if (event.type === 'touchstart') {
+    event.preventDefault()
+  }
   const navHeader = document.querySelector('#nav-header');
   const header = document.querySelector('#header');
   navHeader.classList.toggle('active');
   header.classList.toggle('active')
-});
+} 
+
+
+buttonMenu.addEventListener('click', toggleMenu);
+buttonMenu.addEventListener('touchstart', toggleMenu);
