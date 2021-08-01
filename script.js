846f1422f08757fe661147e9e8291f1e113368de
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-01 20:35:06 -0300

    Adicionando array de projetos e funções

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const buttonMenu = document.querySelector('#btn-mobile');
+const backButton = document.querySelector('#back-button');
+const nextButton = document.querySelector('#next-button');
+let index = 1;
 
 const toggleMenu = (event) => {
   if (event.type === 'touchstart') {
@@ -10,6 +13,50 @@ const toggleMenu = (event) => {
   headerMenu.classList.toggle('active')
 } 
 
-
 buttonMenu.addEventListener('click', toggleMenu);
 buttonMenu.addEventListener('touchstart', toggleMenu);
+
+const projectsArray = [
+  {
+    title: 'TrybeWarts',
+    description: 'Apenas um teste TrybeWarts',
+    button: 'https://michaelcaxias.github.io/projects/trybewarts/',
+  },
+
+  {
+    title: 'Lista de Tarefas',
+    description: 'Apenas um teste Lista de Taregas',
+    button: 'https://michaelcaxias.github.io/projects/todo-list/',
+  }
+];
+
+const nextAndBack = (index) => {
+  const projectIndex = document.querySelector('#project-index')
+  const projectName = document.querySelector('#project-name');
+  const projectDescription = document.querySelector('#project-description');
+  const projectLink = document.querySelector('#project-link');
+  projectIndex.innerHTML = `${index}/5`
+  projectName.innerText = '';
+  projectDescription.innerText = '';
+  projectLink.setAttribute('href', 'https://michaelcaxias.github.io/projects/trybewarts/')
+}
+
+backButton.addEventListener('click', () => {
+  if (index === 1) {
+    index = 5;
+  }
+  else {
+    index -= 1;
+  }
+  nextAndBack(index);
+})
+
+nextButton.addEventListener('click', () => {
+  if (index === 5) {
+    index = 1;
+  }
+  else {
+    index += 1;
+  }
+  nextAndBack(index);
+})
