67c2e3192e4c7db55681c5585c67da90ecaafa74
Michael <michaelcaxias.dev@gmail.com>
2021-11-20 21:39:23 -0300

    Cria Theme

---
+++ src/styles/Theme.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  color: {
+    background: '#ffff',
+    primary: '#FF8DA4',
+    secundary: '#F3FFFF',
+    third: '#EAFAFF',
+    text: '#21243D',
+  },
+};
+
+export const darkTheme = {
+  color: {
+    background: '#2E2534',
+    primary: '#FF8DA4',
+    secundary: '#201925',
+    third: '#201925',
+    text: '#fdfdfd',
+  },
+};
