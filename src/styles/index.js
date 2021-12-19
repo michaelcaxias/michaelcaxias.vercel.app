0939a064b376bd7348a2bbca12a19ac34ba1f8a3
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-12-19 17:55:32 -0300

    Adiciona hover aos botões principais

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,13 +1,22 @@
+/* eslint-disable max-len */
 import styled from 'styled-components';
 
 export const ButtonPrimary = styled.button`
   background-color: #FF8DA4;
   border-radius: 8px;
-  border: 3px solid #FF8DA4;
+  border: none;
   cursor: pointer;
   padding: 8px 24px 8px 24px;
   font-size: 18px;
   font-weight: 600;
+  transition: 0.3s;
+  &:hover {
+    box-shadow: rgba(255,141,164, 0.4) 0px 5px,
+    rgba(255,141,164, 0.3) 0px 10px,
+    rgba(255,141,164, 0.2) 0px 15px,
+    rgba(255,141,164, 0.1) 0px 20px,
+    rgba(255,141,164, 0.05) 0px 25px;
+  }
 `;
 
 export const ButtonSecondary = styled.button`
@@ -18,4 +27,12 @@ export const ButtonSecondary = styled.button`
   padding: 8px 24px 8px 24px;
   font-size: 18px;
   font-weight: 600;
+  transition: 0.3s;
+  &:hover {
+    box-shadow: rgba(255,141,164, 0.4) 0px 5px,
+    rgba(255,141,164, 0.3) 0px 10px,
+    rgba(255,141,164, 0.2) 0px 15px,
+    rgba(255,141,164, 0.1) 0px 20px,
+    rgba(255,141,164, 0.05) 0px 25px;
+  }
 `;
