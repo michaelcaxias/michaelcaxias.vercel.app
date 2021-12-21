70ed6ed290f258e2f3894b154fcc27b710b41837
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-12-21 14:16:56 -0300

    Cria estilização para Card

--- src/sections/Projects/components/Card.jsx
+++ src/sections/Projects/components/Card.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { ButtonPrimary, ButtonSecondary } from '../../../styles/index';
+import example from '../../../images/example.png';
 
 export default function Card() {
   return (
     <section className="card">
-      <section className="card-heading">
-        <img src="" alt="" />
+      <section
+        className="card-heading"
+        style={ { backgroundImage: `url(${example})` } }
+      >
         <h2>Titulo do Projeto</h2>
       </section>
       <section className="card-description">
@@ -14,7 +17,7 @@ export default function Card() {
           aliqua dolor do amet sint. Velit officia consequat
           duis enim velit mollit.
         </p>
-        <section>
+        <section className="buttons-group">
           <ButtonPrimary>Website</ButtonPrimary>
           <ButtonSecondary>Github</ButtonSecondary>
         </section>
--- src/sections/Projects/styles/projects.js
+++ src/sections/Projects/styles/projects.js
@@ -13,8 +13,37 @@ const ProjectsStyle = styled.section`
   .card {
     background-color: white;
     width: 25rem;
-    height: 18rem;
     border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    background-color: ${(props) => props.theme.color.background};
+    color: ${(props) => props.theme.color.text};
+    box-shadow: 5px 5px ${(props) => props.theme.color.primary};
+    .card-heading {
+      border-radius: 8px 8px 0 0;
+      height: 200px;
+      background-size: cover;
+      display: flex;
+      align-items: flex-end;
+      h2 {
+        padding: 0.5rem 0.7rem;
+        color: black;
+        text-shadow: 1px 1px 1px ${(props) => props.theme.color.primary};
+      }
+    }
+    .card-description {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-around;
+      height: 10rem;
+      gap: 1rem;
+      padding: 1.5rem;
+      .buttons-group {
+        display: flex;
+        gap: 1rem;
+      }
+    }
   }
 `;
 
