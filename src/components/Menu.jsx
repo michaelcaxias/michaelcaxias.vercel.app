fed5d44e642718e2c553dc28a887d94b6b679482
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-12-22 16:21:15 -0300

    Implementa onClick ao menuItem

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { MdContentCopy } from 'react-icons/md';
 import { HiOutlineRefresh } from 'react-icons/hi';
-import { BiExit } from 'react-icons/bi';
 import { AiFillLinkedin, AiFillGithub, AiOutlineShareAlt } from 'react-icons/ai';
 
 import useContextMenu from '../hooks/useContextMenu';
@@ -12,14 +11,37 @@ import MenuItem from './MenuItem';
 const Menu = () => {
   const { anchorPoint, show } = useContextMenu();
 
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  const copyLinkToClipboard = () => {
+    navigator.clipboard.writeText(window.location.href);
+  };
+
   return show && (
     <MenuStyle style={ { top: anchorPoint.y, left: anchorPoint.x } }>
-      <MenuItem text="Compartilhar" icon={ <AiOutlineShareAlt /> } />
-      <MenuItem text="Atualizar" icon={ <HiOutlineRefresh /> } />
+      <MenuItem
+        text="Compartilhar"
+        icon={ <AiOutlineShareAlt /> }
+        onClick={ copyLinkToClipboard }
+      />
+      <MenuItem
+        text="Atualizar"
+        onClick={ refreshPage }
+        icon={ <HiOutlineRefresh /> }
+      />
       <MenuItem text="Copiar" icon={ <MdContentCopy /> } />
-      <MenuItem text="Sair" icon={ <BiExit /> } />
-      <MenuItem text="Abrir Linkedin" icon={ <AiFillLinkedin /> } />
-      <MenuItem text="Abrir GitHub" icon={ <AiFillGithub /> } />
+      <MenuItem
+        text="Abrir Linkedin"
+        icon={ <AiFillLinkedin /> }
+        link="https://github.com/michaelcaxias"
+      />
+      <MenuItem
+        text="Abrir GitHub"
+        icon={ <AiFillGithub /> }
+        link="https://br.linkedin.com/in/michaelcaxias"
+      />
     </MenuStyle>
   );
 };
