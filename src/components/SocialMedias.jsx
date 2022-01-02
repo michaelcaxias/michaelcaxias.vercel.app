c812a5d25398f10a85463e21a8efc24af054b0e5
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-02 16:18:29 -0300

    Cria componente SocialMedias.jsx

---
+++ src/components/SocialMedias.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BsInstagram } from 'react-icons/bs';
+
+export default function SocialMedias() {
+  return (
+    <footer>
+      <nav>
+        <a
+          href="https://www.instagram.com/michaelcaxias/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsInstagram />
+        </a>
+      </nav>
+      <p>2021 Ⓒ Desenvolvido por Michael Caxias</p>
+    </footer>
+  );
+}
