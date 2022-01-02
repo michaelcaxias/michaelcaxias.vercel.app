69832d05bdb74af97d9a76cc84bfabc1ebfbd06b
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-02 18:50:40 -0300

    Captura valores dos campos de enviar email

--- src/sections/Footer/components/SendEmail.jsx
+++ src/sections/Footer/components/SendEmail.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@mui/material';
 import { ButtonPrimary } from '../../../styles/index';
 
 export default function SendEmail() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
   return (
     <form className="send-email">
       <TextField
         label="Nome"
+        value={ name }
+        onChange={ ({ target: { value } }) => setName(value) }
         variant="filled"
       />
       <TextField
         label="E-mail"
+        value={ email }
+        onChange={ ({ target: { value } }) => setEmail(value) }
         variant="filled"
       />
       <TextField
         label="Mensagem"
+        value={ message }
         variant="filled"
+        onChange={ ({ target: { value } }) => setMessage(value) }
         multiline
         rows={ 4 }
         maxRows={ 5 }
