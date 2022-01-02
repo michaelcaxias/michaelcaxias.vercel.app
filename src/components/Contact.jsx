28f111ba3afa76ee8e94cca86d75e01f621caaa5
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-02 16:06:07 -0300

    Cria componente Contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <section>
+      <section>
+        <h2>Fale comigo!</h2>
+        <button type="button">WhatsApp</button>
+        <nav>
+          <ul>
+            <li>
+              7michaeel7@gmail.com
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </section>
+  );
+}
