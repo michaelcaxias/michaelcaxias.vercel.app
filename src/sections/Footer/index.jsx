c8944d889d02d48e5cee5fa5b4ef010da46a0717
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-02 16:21:15 -0300

    Altera nome e desenvolve componente 'SocialNetworks'

--- src/sections/Footer/index.jsx
+++ src/sections/Footer/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Contact from '../../components/Contact';
+import SocialNetworks from '../../components/SocialNetworks';
 
 export default function Footer() {
   return (
     <footer>
       <Contact />
+      <SocialNetworks />
     </footer>
   );
 }
