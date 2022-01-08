144c8b7b3e484fa0c93107814ce7660aa2752f49
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-08 14:54:28 -0300

    Ajusta estilizações gerais do styles/index.js

--- src/styles/index.js
+++ src/styles/index.js
@@ -20,15 +20,20 @@ export const ButtonPrimary = styled.button`
 `;
 
 export const ButtonSecondary = styled.button`
+  align-items: center;
   background-color: ${(props) => props.theme.color.background};
   border-radius: 8px;
+  width: 100%;
   border: 2px solid ${(props) => props.theme.color.text};
   color: ${(props) => props.theme.color.text};
   cursor: pointer;
+  display: flex;
   padding: 8px 24px 8px 24px;
   font-size: 18px;
   font-weight: 600;
+  justify-content: center;
   transition: 0.3s;
+  text-align: center;
   &:hover {
     box-shadow: rgba(255,141,164, 0.4) 0px 5px,
     rgba(255,141,164, 0.3) 0px 10px,
