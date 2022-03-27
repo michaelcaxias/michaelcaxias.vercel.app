55675f4b4bec1e724bd3db67a5d3c99749024f03
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-26 22:01:17 -0300

    feat: adiciona funcionamento para fechar modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ModalStyle from '../styles/modal';
+import { MyContext } from '../context/MyProvider';
 
 export default function Modal() {
+  const { handleClose } = useContext(MyContext);
+
+  const closeModal = ({ target }) => {
+    if (target.className === 'sc-iCfMLu jWPEfE') {
+      handleClose();
+    }
+  };
+
   return (
-    <ModalStyle>
+    <ModalStyle onClick={ (event) => closeModal(event) }>
       <section className="modal">
         <h1>Modal</h1>
         <p>modal modal modal</p>
