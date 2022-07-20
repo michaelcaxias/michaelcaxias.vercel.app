9853005990ea0a9cb88cb97d88fdc47288ff5c30
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-07-19 22:39:11 -0300

    :sparkles: feat: refatora cards de projetos

--- src/sections/Projects/index.jsx
+++ src/sections/Projects/index.jsx
@@ -1,26 +1,20 @@
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
 import { BsArrowRightSquare } from 'react-icons/bs';
 import ProjectsStyle from './styles/projects';
 import Card from './components/Card';
-import projectsData from '../../services/projects_data';
 
 export default function Projects() {
   return (
     <ProjectsStyle id="projects">
       <h1 data-aos="fade-up">Projetos</h1>
       <section className="projects-cards">
-        {projectsData && projectsData
-          .map(({ image, title, description, github, website, modalInfo }) => (
-            <Card
-              key={ title }
-              image={ image }
-              title={ title }
-              description={ description }
-              github={ github }
-              website={ website }
-              modalInfo={ modalInfo }
-            />
-          ))}
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
       <a
         href="https://github.com/michaelcaxias?tab=repositories"
