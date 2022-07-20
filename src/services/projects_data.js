cac1cc053720493b0612525907916737cf7cf64d
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-07-19 23:16:09 -0300

    :sparkles: feat: adiciona mesmo projeto duplicado

--- src/services/projects_data.js
+++ src/services/projects_data.js
@@ -1,8 +1,56 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 export default [
   {
     image: 'assets/img/compliance-station-saas.png',
     title: 'Compliance Station - SAAS',
-    description: 'Projeto System as a Service (SAAS) para plataforma Compliance Station.',
+    description: `
+      Projeto SAAS e Rede de Integridade para empresa Compliance Station.
+    `,
+    github: '',
+    website: 'https://compliancestation.com.br/',
+  },
+  {
+    image: 'assets/img/compliance-station-saas.png',
+    title: 'Compliance Station - SAAS',
+    description: `
+      Projeto SAAS e Rede de Integridade para empresa Compliance Station.
+    `,
+    github: '',
+    website: 'https://compliancestation.com.br/',
+  },
+  {
+    image: 'assets/img/compliance-station-saas.png',
+    title: 'Compliance Station - SAAS',
+    description: `
+      Projeto SAAS e Rede de Integridade para empresa Compliance Station.
+    `,
+    github: '',
+    website: 'https://compliancestation.com.br/',
+  },
+  {
+    image: 'assets/img/compliance-station-saas.png',
+    title: 'Compliance Station - SAAS',
+    description: `
+      Projeto SAAS e Rede de Integridade para empresa Compliance Station.
+    `,
+    github: '',
+    website: 'https://compliancestation.com.br/',
+  },
+  {
+    image: 'assets/img/compliance-station-saas.png',
+    title: 'Compliance Station - SAAS',
+    description: `
+      Projeto SAAS e Rede de Integridade para empresa Compliance Station.
+    `,
+    github: '',
+    website: 'https://compliancestation.com.br/',
+  },
+  {
+    image: 'assets/img/compliance-station-saas.png',
+    title: 'Compliance Station - SAAS',
+    description: `
+      Projeto SAAS e Rede de Integridade para empresa Compliance Station.
+    `,
     github: '',
     website: 'https://compliancestation.com.br/',
   },
