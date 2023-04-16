9baa38095ab0dc0cb7b81e1d24e7b0f8ba6361fd
Michael Caxias <michael.caxias@mercadolivre.com>
2023-04-16 17:11:07 -0300

    move provider

---
+++ src/context/MyProvider.tsx
@@ -0,0 +1,46 @@
+import React, { PropsWithChildren, createContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const MyContext = createContext();
+
+export function MyProvider({ children }: PropsWithChildren) {
+  const [darkMode, setDarkMode] = useState(true);
+  const [modalData, setModalData] = useState({
+    title: 'Titulo do Projeto',
+  });
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const setOrCreateThemeStorage = () => {
+      const darkModeStorage = JSON.parse(localStorage.getItem('darkMode'));
+      if (darkModeStorage === null) localStorage.setItem('darkMode', darkMode);
+      return setDarkMode(darkModeStorage);
+    };
+    setOrCreateThemeStorage();
+  }, [darkMode]);
+
+  const context = {
+    darkMode,
+    setDarkMode,
+    isOpen,
+    setOpen,
+    handleOpen,
+    handleClose,
+    modalData,
+    setModalData,
+  };
+
+  return <MyContext.Provider value={context}>{children}</MyContext.Provider>;
+}
+
+MyProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
